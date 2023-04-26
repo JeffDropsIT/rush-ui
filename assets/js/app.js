63e8bd05d7dd0781cd593a6305749c5370c53b3c
jeffdropsit <mathonsiphumzile@gmail.com>
2023-04-26 11:00:20 +0200

    TASK-1: update pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,6 +26,21 @@ $(document).ready(function () {
         $(".m4").addClass("active");
     });
 
+    // Terms & Conditions
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
     // $(".partners").slick();
     $(".partners").slick({
         infinite: true,
